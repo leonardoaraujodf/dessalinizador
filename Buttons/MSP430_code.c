1fc7d0e628f57d960994db64c303fcc3e3cab839
Leonardo Amorim <leonardoaraujodf@gmail.com>
2018-11-17 19:55:23 -0200

    Changes in the rising edge detection in the interrupt of the level sensor

--- Buttons/MSP430_code.c
+++ Buttons/MSP430_code.c
@@ -198,9 +198,9 @@ void turn_LowValve(unsigned char value){
 
 void setupLevelSensor(void){
   P1DIR &= ~LEVEL_SENSOR; //level sensor as input
-  P1IES &= ~LEVEL_SENSOR; //falling edge from LOW TO HIGH
+  P1IES |= LEVEL_SENSOR; //falling edge from HIGH TO LOW
   P1IE |= LEVEL_SENSOR; //Interrupt on Input Pin LEVEL_SENSOR
-  P1IFG |= LEVEL_SENSOR; //Interrupt flag cleared
+  P1IFG &= ~LEVEL_SENSOR; //Interrupt flag cleared
 }
 
 void treat_DataReceived(void){
@@ -275,13 +275,13 @@ interrupt(USCIAB0TX_VECTOR) USCIAB0TX_ISR(void){
 }
 
 interrupt(PORT1_VECTOR) Port_1(void){
-
+/*
   if((P1IN & LEVEL_SENSOR) == 0){
     turn_TopValve(TOP_VALVE_OFF);
     turn_SamplesMotor(SAMPLES_MOTOR_OFF);
 		level_sensor = 1; //level sensor variable is 1, it means that the samples
 //could be taken, and RPI should be advised.
-  }
+}*/
 
   P1IFG &= ~LEVEL_SENSOR;
 }
