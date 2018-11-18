27ffce4bf78f2dc059f17d2c1f01a176df5e8c0b
Leonardo Amorim <leonardoaraujodf@gmail.com>
2018-11-18 10:03:06 -0200

    Changes in the interruption of p1 port

--- Buttons/MSP430_code.c
+++ Buttons/MSP430_code.c
@@ -282,6 +282,5 @@ interrupt(PORT1_VECTOR) Port_1(void){
 		level_sensor = 1; //level sensor variable is 1, it means that the samples
 //could be taken, and RPI should be advised.
 	}
-
   P1IFG &= ~LEVEL_SENSOR;
 }
